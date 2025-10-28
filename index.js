250d060709545c3b86dc920e2ae6bf0c0d5b1642
dutabrahmana <dutalinggau25@gmail.com>
2025-10-28 15:31:25 +0700

    Menambahkan endpoint POST /komik untuk membuat data komik baru. Rute ini menyertakan error handling try/catch dan juga memfinalisasi penanganan error koneksi database awal.

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+db.sequelize.sync().then(() => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+
+ })
+ .catch((err) => {
+    console.log(err);
+ })
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
